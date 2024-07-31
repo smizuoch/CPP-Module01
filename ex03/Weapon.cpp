d284378bbf119a0321751d3cd62e2106329a58b7
smizuoch <shotaro.mizuochi@proton.me>
2024-07-31 14:21:50 +0900

    add ex03

---
+++ ex03/Weapon.cpp
@@ -0,0 +1,15 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(const std::string &initialType) : type(initialType)
+{
+}
+
+const std::string &Weapon::getType() const
+{
+	return type;
+}
+
+void Weapon::setType(const std::string &newType)
+{
+	type = newType;
+}
---
+++ ex03/main.cpp
@@ -0,0 +1,27 @@
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+#include "Weapon.hpp"
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
+
+/*
+HumanA uses a reference to Weapon, ensuring it always has a weapon, while HumanB uses a pointer, allowing for the possibility of not having a weapon.
+*/
